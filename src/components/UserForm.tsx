cd14da9adb28372023e76556e570e35e55e6f618
Vito Chiaradia <vitochiaradia@gmail.com>
2024-08-27 19:30:19 +0200

    add form style

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form';
-import axios from 'axios';
-import { User } from '../lib/types';
-import STRINGS from '../lib/strings';
-import CONST from '../lib/const';
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import { User } from "../lib/types";
+import STRINGS from "../lib/strings";
+import CONST from "../lib/const";
 
 const UserForm: React.FC = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<User>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<User>();
 
   const onSubmit: SubmitHandler<User> = async (data) => {
     try {
       const response = await axios.post(
-        'https://gorest.co.in/public/v2/users',
+        "https://gorest.co.in/public/v2/users",
         {
           name: data.name,
           gender: data.gender,
@@ -20,69 +25,118 @@ const UserForm: React.FC = () => {
         },
         {
           headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${CONST.gorestAuthToken}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${CONST.gorestAuthToken}`,
           },
         }
       );
-      console.log(STRINGS.userSuccessful, response.data);
-    } catch (error) {
-      console.error(STRINGS.userError, error);
+      console.log(response.data);
+
+      alert(STRINGS.userSuccessful);
+    } catch (error: any) {
+      if (axios.isAxiosError(error) && error.response?.status === 422) {
+        const errors = error.response?.data;
+
+        // Handle email already in use
+        if (errors && Array.isArray(errors)) {
+          errors.forEach((err: any) => {
+            if (err.field === "email") {
+              setError("email", {
+                type: "manual",
+                message: STRINGS.emailUsed,
+              });
+            }
+          });
+        }
+      }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Name:</label>
-        <input
-          type="text"
-          {...register('name', { 
-            required: STRINGS.nameRequired, 
-            minLength: { value: 2, message: STRINGS.minName }, 
-            maxLength: { value: 50, message: STRINGS.maxName } 
-          })}
-        />
-        {errors.name && <span>{errors.name.message}</span>}
-      </div>
+    <div className="container">
+      <form className="user-form" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            placeholder="Enter Name"
+            type="text"
+            {...register("name", {
+              required: STRINGS.nameRequired,
+              minLength: { value: 2, message: STRINGS.minName },
+              maxLength: { value: 50, message: STRINGS.maxName },
+            })}
+          />
+          {errors.name && (
+            <span>
+              <i className="fa-solid fa-circle-exclamation"></i>
+              {errors.name.message}
+            </span>
+          )}
+        </div>
 
-      <div>
-        <label>Gender:</label>
-        <select {...register('gender', { required: STRINGS.genderRequired })}>
-          <option value="">Seleziona...</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="female">Not specify</option>
-        </select>
-        {errors.gender && <span>{errors.gender.message}</span>}
-      </div>
+        <div>
+          <input
+            placeholder="Enter Email"
+            type="email"
+            {...register("email", {
+              required: STRINGS.emailRequired,
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: STRINGS.emailInvalid,
+              },
+            })}
+          />
+          {errors.email && (
+            <span>
+              <i className="fa-solid fa-circle-exclamation"></i>
+              {errors.email.message}
+            </span>
+          )}
+        </div>
 
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          {...register('email', { 
-            required: STRINGS.emailRequired, 
-            pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: STRINGS.emailInvalid } 
-          })}
-        />
-        {errors.email && <span>{errors.email.message}</span>}
-      </div>
+        <div>
+          <select
+            defaultValue=""
+            {...register("gender", { required: STRINGS.genderRequired })}
+          >
+            <option value="" disabled hidden>
+              Select gender...
+            </option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="female">Not specify</option>
+          </select>
+          {errors.gender && (
+            <span>
+              <i className="fa-solid fa-circle-exclamation"></i>
+              {errors.gender.message}
+            </span>
+          )}
+        </div>
 
-      <div>
-        <label>Status:</label>
-        <select {...register('status', { required: STRINGS.statusRequired })}>
-          <option value="">Seleziona...</option>
-          <option value="active">Active</option>
-          <option value="inactive">Inactive</option>
-        </select>
-        {errors.status && <span>{errors.status.message}</span>}
-      </div>
+        <div>
+          <select
+            defaultValue=""
+            {...register("status", { required: STRINGS.statusRequired })}
+          >
+            <option disabled hidden value="">
+              Select status...
+            </option>
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
+          </select>
+          {errors.status && (
+            <span>
+              <i className="fa-solid fa-circle-exclamation"></i>
+              {errors.status.message}
+            </span>
+          )}
+        </div>
 
-      <button type="submit">Sign Up</button>
-    </form>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
-}
- 
+};
+
 export default UserForm;
